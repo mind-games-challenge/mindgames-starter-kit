9658ce1154c5819273933cebee67b8c157946623
ThatParticularPencil <megamer30.000@gmail.com>
2025-07-27 15:06:23 -0500

    this is really broken but I had to commit in haste lol

--- src/offline_play.py
+++ src/offline_play.py
@@ -4,12 +4,14 @@ You (human player) will be player 0, and the AI model will be player 1.
 """
 
 import textarena as ta 
-from agent import HumanAgent
+from agent import HumanAgent, RandomAgent, LLMAgent
 
 # initialize the agents
 agents = {
     0: HumanAgent(),
-    1: ta.agents.OpenRouterAgent(model_name="google/gemini-2.0-flash-lite-001"),
+    # 1: RandomAgent(),
+    # 1: ta.agents.OpenRouterAgent(model_name="google/gemini-2.0-flash-lite-001"),
+    1: LLMAgent("google/gemini-2.0-flash-lite-001")
 }
 
 # initialize the environment
@@ -19,9 +21,14 @@ env.reset(num_players=len(agents))
 # main game loop
 done = False 
 while not done:
-  player_id, observation = env.get_observation()
-  action = agents[player_id](observation)
-  done, step_info = env.step(action=action)
+    player_id, observation = env.get_observation()
+    response = agents[player_id](observation)         #response will be a list
+    action = response[0]
+    if len(response) > 1:                             #allows agents to return two items
+        secondary = response
+        print(f"\nsubmitted: {action}")
+        print(f"additional return from agent {player_id}:\n {secondary} \n")   #intended to diagnose screw ups
+    done, step_info = env.step(action = action) #! keep in mind that you get two chances to submit a valid response
 rewards, game_info = env.close()
 
 print(f"Rewards: {rewards}")
